1bc010e00216803be74a053c547dba0c61385041
waaker <anael.moissiard@gmail.com>
2020-03-19 16:37:54 +0100

    test(backend/model): start tests for account

---
+++ server/account/model.test.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const config = require('config')
+const bcrypt = require('bcrypt')
+const { model: Account } = require('.')
+
+beforeAll(async () => {
+  await mongoose.connect(`mongodb://${config.get('Database.uri')}:${config.get('Database.port')}/${config.get('Database.name')}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
+
+afterEach(async () => {
+  await Account.deleteAccounts()
+})
+
+describe('insert', () => {
+  it('should create an account', async () => {
+    const mockAccount = {
+      accountName: 'myTestAccount',
+      password: 'myTestPassword',
+      ftpsServers: []
+    }
+
+    const mongooseAccount = await Account.createAccount(mockAccount)
+
+    expect(mongooseAccount.accountName).toEqual(mockAccount.accountName)
+    expect(bcrypt.compareSync(mockAccount.password, mongooseAccount.hash)).toBe(true)
+  }, 10000)
+})
