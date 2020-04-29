68d0e7eae8f7916ff7c6fb12b447afe9bf6e7a23
waaker <anael.moissiard@gmail.com>
2020-04-29 15:52:13 +0200

    build/fix(backend): replace 'ftp' package by 'basic-ftp' + npm update

--- backend/src/utils/ftps.js
+++ backend/src/utils/ftps.js
@@ -1,4 +1,4 @@
-const Client = require('ftp')
+const ftp = require('basic-ftp')
 const fs = require('fs')
 
 const { model: FtpsServer } = require('../ftps_server')
@@ -7,40 +7,43 @@ var ftpsClients = new Map()
 
 const connect = async (req, res, next) => {
   const ftpsServer = await FtpsServer.getFtpsServer(req.params.serverId)
-  const ftpsClient = new Client()
+  const ftpsClient = new ftp.Client()
 
-  ftpsClient.connect({
-    host: ftpsServer.host,
-    port: ftpsServer.port,
-    user: ftpsServer.user,
-    password: ftpsServer.password,
-    secure: true,
-    secureOptions: {
-      ca: [fs.readFileSync(ftpsServer.certificate_path)]
+  try {
+    const resp = await ftpsClient.access({
+      host: ftpsServer.host,
+      port: ftpsServer.port,
+      user: ftpsServer.user,
+      password: ftpsServer.password,
+      secure: true,
+      secureOptions: {
+        ca: [fs.readFileSync(ftpsServer.certificate_path)]
+      }
+    })
+
+    ftpsClients.set(req.params.serverId, ftpsClient)
+
+    if (resp.code === 220) {
+      res.status(200).send({ connected: true })
+    } else {
+      throw new Error('Response code is not 220')
     }
-  })
-
-  ftpsClients.set(req.params.serverId, ftpsClient)
-
-  ftpsClient.status(function (err, status) {
-    if (err) {
-      res.status(500).json(JSON.stringify(err, Object.getOwnPropertyNames(err)))
-    }
-    res.status(200).send({ connected: true })
-  })
+  } catch (err) {
+    res.status(500).json(JSON.stringify(err, Object.getOwnPropertyNames(err)))
+  }
 }
 
 const isConnected = (req, res, next) => {
   if (ftpsClients.has(req.params.serverId)) {
-    return res.status(200).json({ isConnected: true })
+    return next()
   } else {
     return res.status(200).json({ isConnected: false })
   }
 }
 
-const disconnect = (req, res, next) => {
+const disconnect = async (req, res, next) => {
   try {
-    ftpsClients.get(req.params.serverId).end()
+    await ftpsClients.get(req.params.serverId).close()
     ftpsClients.delete(req.params.serverId)
     res.status(200).send({ connected: false })
   } catch (err) {
@@ -50,10 +53,8 @@ const disconnect = (req, res, next) => {
 
 const list = async (req, res, next) => {
   try {
-    ftpsClients.get(req.params.serverId).list(req.body.path, function (err, list) {
-      if (err) { throw err }
-      res.status(200).send(list)
-    })
+    const list = await ftpsClients.get(req.params.serverId).list(req.body.path)
+    res.status(200).send(list)
   } catch (err) {
     res.status(500).json(JSON.stringify(err, Object.getOwnPropertyNames(err)))
   }
