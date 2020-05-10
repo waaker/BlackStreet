d2452f11ce0802bc705c51c2dcc30c2881fbd8c1
waaker <anael.moissiard@gmail.com>
2020-05-10 16:21:04 +0200

    fix/test(frontend): add missing dependencies for admin component tests

--- frontend/src/app/admin/admin.component.spec.ts
+++ frontend/src/app/admin/admin.component.spec.ts
@@ -1,4 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AdminComponent } from './admin.component';
 
@@ -8,7 +17,19 @@ describe('AdminComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminComponent ]
+      declarations: [ AdminComponent ],
+      imports: [
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        MatExpansionModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatSnackBarModule,
+        MatTabsModule
+      ]
     })
     .compileComponents();
   }));
